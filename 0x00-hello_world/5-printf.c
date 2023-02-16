82e59730ef0c200858a83798fe08c89f4ade63c6
EvansOdhams <evansodhams@gmail.com>
2023-02-16 09:06:42 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * name - A program that prints a line using prinf function.
+ * Return: 0 (A success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
