c5e236d3089535861cab40ad6ff35a7d5b0b0bf5
EvansOdhams <evansodhams@gmail.com>
2023-03-02 09:55:04 +0300

    function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 
 /**
-* main - check the code
+* main - Entry point
 *
-* Return: Always 0.
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-	char str[] = "Expect the best. Prepare for the worst."
-	"\nCapitalize on what comes."
-	"\nhello world! hello-world 0123456hello world\thello world."
-	"hello world\n";
-	char *ptr;
+char str[] = "Expect the best. Prepare for the worst.\n"
+"Capitalize on what comes.\n"
+"hello world! hello-world 0123456hello world\thello world.\n"
+"hello world\n";
+char *ptr;
 
-	ptr = cap_string(str);
-	printf("%s", ptr);
-	printf("%s", str);
-	return (0);
+ptr = cap_string(str);
+printf("%s", ptr);
+printf("%s", str);
+
+return (0);
 }
