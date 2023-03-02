d3a98df3619f00b35e0a9c19db1e973893d3ba45
EvansOdhams <evansodhams@gmail.com>
2023-03-02 22:30:14 +0300

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+int n;
+int a[5];
+int *p;
 
-	a[2] = 1024;
-	p = &n;
+a[2] = 1024;
+p = &n;
 /*
 * write your line of code here...
 * Remember:
+* main - magic
 * - you are not allowed to use a
 * - you are not allowed to modify p
 * - only one statement
