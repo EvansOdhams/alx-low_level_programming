f585d147cee3d72ee90eca64a48e1b9cd757404a
EvansOdhams <evansodhams@gmail.com>
2023-03-10 19:50:15 +0300

    Updated

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _puts - prints a string to the standard output
+ * @s: pointer to the string to be printed
+ *
+ * Return: void
+ */
+void _puts(char *s)
+{
+puts(s);
+}
+
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+* _putchar - writes a character to the standard output
+* @c: the character to be written
+*
+* Return: On success, the character written. On error, -1 is returned
+*/
+int _putchar(char c)
+{
+return (putchar(c));
+}
+
