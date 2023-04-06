b1e9becf019d044cb8fe205dda71a2a14dc44025
EvansOdhams <evansodhams@gmail.com>
2023-04-06 19:36:18 +0300

    Add prototypes for various bit manipulation functions in main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/* Prototype for _putchar function */
+int _putchar(char c);
+/* Prototype for a function that converts a binary number to an unsigned int*/
+unsigned int binary_to_uint(const char *b);
+/* Protottype for a function that prints the binary */
+void print_binary(unsigned long int n);
+/* Prototype for a function that returns the value of a bit at a given index*/
+int get_bit(unsigned long int n, unsigned int index);
+/* Prototype for a function that sets the value of a bit to 1*/
+int set_bit(unsigned long int *n, unsigned int index);
+/*Prototype for a function that sets the value of a bit to 0 at a given index*/
+int clear_bit(unsigned long int *n, unsigned int index);
+/*Prototype for a function that returns the number of bits*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+#endif /* MAIN_H */
