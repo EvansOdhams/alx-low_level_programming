97177f4d65a74cc2b5cda3303744a24cdb55a799
EvansOdhams <evansodhams@gmail.com>
2023-04-07 19:35:23 +0300

    main.h updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
 
-/* Prototype for a function that reads a text file and prints*/
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
 /*Prototype for a function that creates a file.*/
 int create_file(const char *filename, char *text_content);
