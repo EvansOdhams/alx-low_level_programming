650c58486bcf3dcb202359805881b6e84434ed72
EvansOdhams <evansodhams@gmail.com>
2023-04-07 19:22:52 +0300

    Prototype file main.h updated

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
-/* Prototype for a function that reads a text file and prints it to the POSIX standard output.*/
+/* Prototype for a function that reads a text file and prints*/
 ssize_t read_textfile(const char *filename, size_t letters);
 /*Prototype for a function that creates a file.*/
 int create_file(const char *filename, char *text_content);
