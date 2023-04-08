5e328568b7af79cb53e58861362e284166c7f284
EvansOdhams <evansodhams@gmail.com>
2023-04-08 08:18:02 +0300

    a program that copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,4 +10,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 /*Prototype for a function that appends text at the end of a file.*/
 int append_text_to_file(const char *filename, char *text_content);
+/*Prototype for a a program that copies the content of a file to another file*/
+int cp(const char *file_from, const char *file_to);
 #endif /* MAIN_H */
