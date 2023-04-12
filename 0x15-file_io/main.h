df1356cb056bcf33a0deb3e2aea8f800839949e7
EvansOdhams <evansodhams@gmail.com>
2023-04-12 20:15:40 +0300

    changes made on the prototypes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,4 +12,5 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 /*Prototype for a a program that copies the content of a file to another file*/
 int cp(const char *file_from, const char *file_to);
+void print_elf_header(const char *file);
 #endif /* MAIN_H */
