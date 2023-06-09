96ac7827e56018995419c4b4d24e570a08ec5ffe
EvansOdhams <evansodhams@gmail.com>
2023-06-09 14:08:33 +0300

    Add  a hash function implementing the djb2 algorithm.

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
 * hash_djb2 - Hash function using the djb2 algorithm
 * @str: The string to hash
@@ -9,10 +11,10 @@ unsigned long int hash_djb2(const unsigned char *str)
 unsigned long int hash = 5381;
 int c;
 
+/* djb2 hash algorithm */
 while ((c = *str++))
 {
-hash = ((hash << 5) + hash) ^ c; /* hash * 33 XOR c */
+hash = ((hash << 5) + hash) +c; /* hash * 33 + c */
 }
-
 return (hash);
 }
